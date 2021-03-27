9ac18b2d01bc314e48df709bde45097fb75a55dc
Andrew Bennett <andrewbgm@gmail.com>
2021-03-27 00:44:15 +0000

    react-swing: add onAction handler

--- packages/react-swing/src/components/button/index.tsx
+++ packages/react-swing/src/components/button/index.tsx
@@ -1,6 +1,10 @@
 import { createElement, ReactNode } from 'react'
 
+export type ButtonActionHandler = () => void
+
 export interface ButtonProps {
+  onAction?: ButtonActionHandler
+
   children?: ReactNode
 }
 
