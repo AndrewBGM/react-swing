dffe0b7ea7b6d6e058db6c3182b6ef85af858818
Andrew Bennett <andrewbgm@gmail.com>
2021-03-27 00:31:34 +0000

    react-swing: compile d.ts files into single file

--- packages/react-swing/rollup.config.js
+++ packages/react-swing/rollup.config.js
@@ -1,22 +1,42 @@
 import commonjs from '@rollup/plugin-commonjs'
+import del from 'rollup-plugin-delete'
+import dts from 'rollup-plugin-dts'
 import typescript from 'rollup-plugin-typescript2'
 import pkg from './package.json'
 
-export default {
-  input: 'src/index.ts',
-  output: {
-    file: pkg.main,
-    format: 'cjs',
-    sourcemap: true,
+export default [
+  {
+    input: 'src/index.ts',
+    output: {
+      file: pkg.main,
+      format: 'cjs',
+      sourcemap: true,
+    },
+    plugins: [
+      commonjs(),
+      typescript({
+        useTsconfigDeclarationDir: true,
+      }),
+    ],
+    external: [
+      ...Object.keys(pkg.dependencies),
+      ...Object.keys(pkg.peerDependencies),
+    ],
   },
-  plugins: [
-    commonjs(),
-    typescript({
-      useTsconfigDeclarationDir: true,
-    }),
-  ],
-  external: [
-    ...Object.keys(pkg.dependencies),
-    ...Object.keys(pkg.peerDependencies),
-  ],
-}
+  {
+    input: './dist/@types/index.d.ts',
+    output: [
+      {
+        file: 'dist/index.d.ts',
+        format: 'cjs',
+      },
+    ],
+    plugins: [
+      dts(),
+      del({
+        hook: 'buildEnd',
+        targets: 'dist/@types',
+      }),
+    ],
+  },
+]
