9808d26102c1c26139b7609b09214506b72859e9
Andrew Bennett <andrewbgm@gmail.com>
2021-03-29 01:35:05 +0100

    remove minimist

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
-import parseOptions from 'minimist'
 import type { ReactElement } from 'react'
 import ReactReconciler from 'react-reconciler'
 import Bridge, { BridgeOptions } from './server/bridge'
@@ -9,21 +8,11 @@ const defaultOptions: BridgeOptions = {
 
 const noop = () => {}
 
-const openBridge = async (options: BridgeOptions): Promise<Bridge> => {
+export const render = async (element: ReactElement, options: BridgeOptions = defaultOptions) => {
   const bridge = new Bridge(options)
   await bridge.connect()
 
-  return bridge
-}
-
-export const render = async (element: ReactElement, argv: string[] = []) => {
-  const options = parseOptions<BridgeOptions>(argv, {
-    default: defaultOptions,
-  })
-
-  const bridge = await openBridge(options)
   const ReactSwing = ReactReconciler(bridge)
-
   const rootContainer = ReactSwing.createContainer(0, 0, false, null)
   ReactSwing.updateContainer(element, rootContainer, null, noop)
 }
